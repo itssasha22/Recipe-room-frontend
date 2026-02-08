093414610eeadef9f4aba690076e2c0f735a0246
sasha lisa <lisasashaa@gmail.com>
2026-02-09 00:33:49 +0300

    Update Kenyan group member avatars to Black people

--- src/pages/GroupDetail.jsx
+++ src/pages/GroupDetail.jsx
@@ -40,13 +40,13 @@ const GroupDetail = () => {
       image: 'https://images.unsplash.com/photo-1604909052743-94e838986d24?w=800',
       members: [
         { id: 13, name: 'Chef Wanjiku', role: 'Admin', avatar: 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce?w=100', recipes: 4 },
-        { id: 14, name: 'Kamau Njoroge', role: 'Member', avatar: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=100', recipes: 1 },
-        { id: 15, name: 'Akinyi Odhiambo', role: 'Member', avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=100', recipes: 0 },
-        { id: 16, name: 'Mwangi Kariuki', role: 'Member', avatar: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=100', recipes: 0 },
-        { id: 17, name: 'Njeri Wambui', role: 'Member', avatar: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=100', recipes: 0 },
-        { id: 18, name: 'Otieno Omondi', role: 'Member', avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100', recipes: 0 },
-        { id: 19, name: 'Wangari Muthoni', role: 'Member', avatar: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=100', recipes: 0 },
-        { id: 20, name: 'Kipchoge Kimani', role: 'Member', avatar: 'https://images.unsplash.com/photo-1463453091185-61582044d556?w=100', recipes: 0 }
+        { id: 14, name: 'Kamau Njoroge', role: 'Member', avatar: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=100', recipes: 1 },
+        { id: 15, name: 'Akinyi Odhiambo', role: 'Member', avatar: 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce?w=100', recipes: 0 },
+        { id: 16, name: 'Mwangi Kariuki', role: 'Member', avatar: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=100', recipes: 0 },
+        { id: 17, name: 'Njeri Wambui', role: 'Member', avatar: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?w=100', recipes: 0 },
+        { id: 18, name: 'Otieno Omondi', role: 'Member', avatar: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=100', recipes: 0 },
+        { id: 19, name: 'Wangari Muthoni', role: 'Member', avatar: 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce?w=100', recipes: 0 },
+        { id: 20, name: 'Kipchoge Kimani', role: 'Member', avatar: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=100', recipes: 0 }
       ],
       recipes: [
         { id: 16, name: 'Samosas', author: 'Chef Wanjiku', image: 'https://images.unsplash.com/photo-1601050690597-df0568f70950?w=400' },
