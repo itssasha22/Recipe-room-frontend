8076a90a8535e91c169619265f3992c9d5a33f39
sasha lisa <lisasashaa@gmail.com>
2026-02-08 10:36:29 +0300

    Fix app structure: Add routing, Redux provider, and basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './App.css';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import CreateRecipe from './pages/CreateRecipe';
+import RecipesPage from './pages/RecipesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Recipe Room</h1>
-        <p>Share and discover simple recipes</p>
-      </header>
-    </div>
-  )
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <header>
+            <h1>Recipe Room</h1>
+            <p>Share and discover simple recipes</p>
+          </header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/recipes" element={<RecipesPage />} />
+            <Route path="/create-recipe" element={<CreateRecipe />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
