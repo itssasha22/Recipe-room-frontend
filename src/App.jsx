ef87a4919ab834624ddf358de014982539852615
sasha lisa <lisasashaa@gmail.com>
2026-02-08 17:27:22 +0300

    Fix JSX syntax error: Rename .js files to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import './App.css';
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import CreateRecipe from './pages/CreateRecipe';
-import RecipesPage from './pages/RecipesPage';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import CreateRecipe from './pages/CreateRecipe.jsx';
+import RecipesPage from './pages/RecipesPage.jsx';
 
 function App() {
   return (
